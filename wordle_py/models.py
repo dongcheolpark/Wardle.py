03643fe7abc65fb8d68e9a1d2122c3e3bbaf227b
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-02-25 20:12:03 +0900

    <추가> 체크함수 반절 완성

--- wordle_py/models.py
+++ wordle_py/models.py
@@ -8,9 +8,22 @@ class AnswerString:
         self.text = []
         return
 
+    def getTextList(self):
+        return self.text
+
     @abstractmethod
     def toString(self):
         return
+
+    def CheckString(self, string):
+        res = [2, 0, 2, 0, 2]
+        _text = string.getTextList()
+        for i in range(len(_text)):
+            if _text[i] == self.text[i]:
+                res[i] = 2;
+            else:
+                res[i] = 0;
+        return res
 
 
 class AnswerStringEnglish(AnswerString):
