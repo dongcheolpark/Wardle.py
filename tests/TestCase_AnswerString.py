1a63a90a8630c226b73f85c9bc356de34000a945
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-02-25 20:13:56 +0900

    <추가> 테스트케이스 추가

--- tests/TestCase_AnswerString.py
+++ tests/TestCase_AnswerString.py
@@ -18,3 +18,14 @@ class TestCaseStart(unittest.TestCase):
         self.assertEqual(res[2], 2)
         self.assertEqual(res[3], 0)
         self.assertEqual(res[4], 2)
+
+    def test_AnswerStringIsSame(self):
+        answer_string = models.AnswerStringEnglish("react")
+        supposition_string = models.AnswerStringEnglish("rcast")
+        res = answer_string.CheckString(supposition_string)
+        self.assertEqual(res[0], 2)
+        self.assertEqual(res[1], 1)
+        self.assertEqual(res[2], 2)
+        self.assertEqual(res[3], 0)
+        self.assertEqual(res[4], 2)
+
