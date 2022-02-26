e722a4087b02bc98fb2efeee80acba51cca6fbee
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-02-26 16:38:56 +0900

    <추가> wordle 클래스 테스트케이스 추가

---
+++ tests/TestCase_wordle.py
@@ -0,0 +1,21 @@
+import unittest
+
+import wordle_py as wd
+import wordle_py.models as md
+
+
+class TestCaseWordle(unittest.TestCase):
+
+    def setUp(self):
+        self.wordle = wd.wordle.WordleOriginal()
+        self.wordle.setAnswer("roast")
+
+    def Test_CheckTurn(self):
+        i = 0
+        answer = md.AnswerStringEnglish('hello')
+        while self.wordle.getTurn():
+            self.wordle.checkString(answer)
+            i += 1
+        self.assertEqual(6, i)
+        return
+
